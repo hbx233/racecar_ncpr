c68de995054c9f0e45a83f100a80f86a165a2ab1
wren2 <wren2@jhu.edu>
2019-05-02 23:04:15 -0400

    local planner

--- src/local_planner.cpp
+++ src/local_planner.cpp
@@ -6,19 +6,44 @@
 namespace ncpr {
 LocalPlanner::LocalPlanner(const ros::NodeHandle& nh)
 {
-  // Get parameters 
+  // Get parameters
   nh_.getParam("car/length");
-  
-  // Connect all the subscribers and publishers 
+
+  // Connect all the subscribers and publishers
   sub_pose_tracking_ = nh_.subscribe(topic_name_[tracking]);
-  
-  // Create PolyTrajectory Instance 
+
+  // Create PolyTrajectory Instance
   local_trajectory_ptr_ = std::make_shared<PolyTrajectory<double,OUTPUT,BASIS>>();
-  
+
   // Create TrajectoryTracker
   local_traj_tracker_ptr_ = std::make_shared<TrajectoryTracker>(local_traj_tracker_ptr_);
 }
 
+void LocalPlanner::run()
+{
 
-  
+}
+
+void LocalPlanner::globalPath_callback(const nav_msgs::Path& msg)
+{
+  if(msg.path.size() != 0){
+    ROS_INFO("receving Global path...");
+    global_path_ = msg;
+    state_ = State::Running;
+  }
+  else{
+    ROS_ERROR("Empty Global path !");
+  }
+
+}
+
+void LocalPlanner::pose_trajectory_callback(const geometry_msgs::Pose& msg)
+{
+  if(state_==State::Running){
+    //Running,
+  }
+}
+
+
+
 }
--- src/tf_listener.cpp
+++ src/tf_listener.cpp
@@ -1,16 +1,27 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "nav_msgs/Odometry.h"
 
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+  ROS_INFO("receving Odometry...");
+  double state_x = msg.pose.pose.positon.x;
+  double state_y = msg.pose.pose.positon.y;
+
+  double vel_x = msg.twist.twist.linear.x;
+  double vel_y =msg.twist.twist.linear.y;
+  double w = msg.twist.twist.angular.z;
+
+  double vel = sqrt(vel_x*vel_x + vel_y*vel_y);
+  double theta = atan2(vel_y,vel_x);
+
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "listener");
+  ros::init(argc, argv, "odom_listener");
   ros::NodeHandle nh_;
-  ros::Subscriber sub = nh_.subscribe("car_state", 1000, state_callback);
+  ros::Subscriber sub = nh_.subscribe("vesc/odom", 1000, odomCallback);
   ros::spin();
 
   return 0;
